86ad5c1e10c1e43e48773b4dc858aa8fd8c1ba7c
st1ko MAIN USER <st1ko MAIN USER@DESKTOP-3DONU29>
2023-06-18 16:08:40 +0300

    Add project files.

---
+++ network python test/network_python_test.py
@@ -0,0 +1,14 @@
+name = ""
+
+#build a AI barista
+
+print ("hello welcome to st1kos coffe shop")
+
+name = input ("what is your name?\n")
+
+print ("hello " + name + (" what would you like to order?"))
+
+print ("I will take a large black coffe")
+print ("alright coming up soon")
+print ("your coffe is ready " + name)
+print ("thank you")
