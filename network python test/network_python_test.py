4db2485c9f896218306b1d7b48667fd10a3d55c1
st1ko-gs <antonluomala2008@gmail.com>
2023-06-19 14:04:46 +0300

    added a name block, if name shinno, exit()

--- network python test/network_python_test.py
+++ network python test/network_python_test.py
@@ -1,5 +1,8 @@
 # first we are going to add a variable named "name" as you can see to make a variable you just need to type the name you want to make it and = 
 
+from ast import If
+
+
 name = ""
 #this variable will show a list of active drinks, note we will use this to save the info for the barista
 coffelist = "latte coffees , black coffees , milk coffees , "
@@ -12,16 +15,25 @@ print ("hello welcome to st1kos coffe shop")
 #use the variable to get the barista to let you input the name you want
 
 name = input ("what is your name?\n")
+
+if name == "shinno":
+    print ("you are not welcome here get out")
+    exit(0)
+else:
+ print ("hello " + name + (" what would you like to order?"))
+
+
 # added a + so the barista will repeat the name you entered 
-print ("hello " + name + (" what would you like to order?"))
+#print ("hello " + name + (" what would you like to order?"))
+
 print ("can i see your list")
 
 
 #the input does so we can type in to the barista what coffe we want
-
 coffelist = input ("here is our list: " + coffelist )
 
-
+#
+#print (" ")
 
 #will take the info we just typed for the barista and print it out 
 
