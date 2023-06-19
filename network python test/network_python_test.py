7596d4a85b587966cbd290099a86816c524fd60f
st1ko-gs <antonluomala2008@gmail.com>
2023-06-19 09:49:38 +0300

    added a coffelist and documentation

--- network python test/network_python_test.py
+++ network python test/network_python_test.py
@@ -1,15 +1,2 @@
 # first we are going to add a variable named "name" as you can see to make a variable you just need to type the name you want to make it and = 
-name = ""
 
-#build a AI barista
-#print the first word the barista will say
-print ("hello welcome to st1kos coffe shop")
-#use the variable to get the barista to let you input the name you want
-name = input ("what is your name?\n")
-# added a + so the barista will repeat the name you entered 
-print ("hello " + name + (" what would you like to order?"))
-
-print ("I will take a large black coffe")
-print ("alright coming up soon")
-print ("your coffe is ready " + name)
-print ("thank you")
